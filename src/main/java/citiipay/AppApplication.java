14694c93b715543782daf3199778ff3dfbabb8b1
ChainsysTraining <naresh.20191224@gmail.com>
2020-02-22 14:19:20 +0530

    Update AppApplication.java

--- src/main/java/citiipay/AppApplication.java
+++ src/main/java/citiipay/AppApplication.java
@@ -5,9 +5,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 @SpringBootApplication(exclude=DataSourceAutoConfiguration.class)
 @ServletComponentScan
-public class AppApplication {
+public class AppApplication  extends SpringBootServletInitializer{
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(AppApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppApplication.class, args);
