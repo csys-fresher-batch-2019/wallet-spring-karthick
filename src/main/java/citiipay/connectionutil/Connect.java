394ba41297f8eedc3d8078544ad00350abe60632
ChainsysTraining <naresh.20191224@gmail.com>
2020-02-22 14:54:32 +0530

    Update Connect.java

--- src/main/java/citiipay/connectionutil/Connect.java
+++ src/main/java/citiipay/connectionutil/Connect.java
@@ -2,12 +2,15 @@ package citiipay.connectionutil;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.util.TimeZone;
 
 public class Connect {
 
 	public static Connection connect() throws Exception {
 		Class.forName("oracle.jdbc.driver.OracleDriver");
 		String server = "13.235.147.120";// "localhost";
+		TimeZone timeZone = TimeZone.getTimeZone("Asia/Kolkata");
+			TimeZone.setDefault(timeZone);
 		Connection connection = DriverManager.getConnection("jdbc:oracle:thin:@" + server + ":1521:XE", "system",
 				"oracle");
 		return connection;
